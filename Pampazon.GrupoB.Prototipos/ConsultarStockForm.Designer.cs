f99d2620df3d136d7389c17060ce489d09e0f8f3
NicoCalabre <115898243+NicoCalabre@users.noreply.github.com>
2024-05-18 15:58:26 -0300

    Cambios Gestionar Stock

--- Pampazon.GrupoB.Prototipos/ConsultarStockForm.Designer.cs
+++ Pampazon.GrupoB.Prototipos/ConsultarStockForm.Designer.cs
@@ -28,12 +28,233 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ConsultarStockForm";
+            LabelIDCliente = new Label();
+            TxtIDCliente = new TextBox();
+            TxtDescripcionCliente = new TextBox();
+            LabelDescripcionCliente = new Label();
+            TxtIDProducto = new TextBox();
+            LabelIDProducto = new Label();
+            TxtDescripcionProducto = new TextBox();
+            LabelDescripcionProducto = new Label();
+            ComboEstado = new Label();
+            TxtPrioridad = new TextBox();
+            LabelPrioridad = new Label();
+            TxtPosicion = new TextBox();
+            LabelPosicion = new Label();
+            TxtEspacioDisponible = new TextBox();
+            LabelEspacioDisponible = new Label();
+            BotonVolver = new Button();
+            BotonBuscar = new Button();
+            BotonLimpiar = new Button();
+            ComboBoxEstado = new ComboBox();
+            SuspendLayout();
+            // 
+            // LabelIDCliente
+            // 
+            LabelIDCliente.AutoSize = true;
+            LabelIDCliente.Location = new Point(178, 110);
+            LabelIDCliente.Name = "LabelIDCliente";
+            LabelIDCliente.Size = new Size(58, 15);
+            LabelIDCliente.TabIndex = 0;
+            LabelIDCliente.Text = "ID Cliente";
+            // 
+            // TxtIDCliente
+            // 
+            TxtIDCliente.Location = new Point(178, 128);
+            TxtIDCliente.Name = "TxtIDCliente";
+            TxtIDCliente.Size = new Size(212, 23);
+            TxtIDCliente.TabIndex = 1;
+            // 
+            // TxtDescripcionCliente
+            // 
+            TxtDescripcionCliente.Location = new Point(178, 172);
+            TxtDescripcionCliente.Name = "TxtDescripcionCliente";
+            TxtDescripcionCliente.Size = new Size(212, 23);
+            TxtDescripcionCliente.TabIndex = 3;
+            // 
+            // LabelDescripcionCliente
+            // 
+            LabelDescripcionCliente.AutoSize = true;
+            LabelDescripcionCliente.Location = new Point(178, 154);
+            LabelDescripcionCliente.Name = "LabelDescripcionCliente";
+            LabelDescripcionCliente.Size = new Size(109, 15);
+            LabelDescripcionCliente.TabIndex = 2;
+            LabelDescripcionCliente.Text = "Descripción Cliente";
+            // 
+            // TxtIDProducto
+            // 
+            TxtIDProducto.Location = new Point(178, 216);
+            TxtIDProducto.Name = "TxtIDProducto";
+            TxtIDProducto.Size = new Size(212, 23);
+            TxtIDProducto.TabIndex = 5;
+            // 
+            // LabelIDProducto
+            // 
+            LabelIDProducto.AutoSize = true;
+            LabelIDProducto.Location = new Point(178, 198);
+            LabelIDProducto.Name = "LabelIDProducto";
+            LabelIDProducto.Size = new Size(70, 15);
+            LabelIDProducto.TabIndex = 4;
+            LabelIDProducto.Text = "ID Producto";
+            // 
+            // TxtDescripcionProducto
+            // 
+            TxtDescripcionProducto.Location = new Point(178, 260);
+            TxtDescripcionProducto.Name = "TxtDescripcionProducto";
+            TxtDescripcionProducto.Size = new Size(212, 23);
+            TxtDescripcionProducto.TabIndex = 7;
+            // 
+            // LabelDescripcionProducto
+            // 
+            LabelDescripcionProducto.AutoSize = true;
+            LabelDescripcionProducto.Location = new Point(178, 242);
+            LabelDescripcionProducto.Name = "LabelDescripcionProducto";
+            LabelDescripcionProducto.Size = new Size(121, 15);
+            LabelDescripcionProducto.TabIndex = 6;
+            LabelDescripcionProducto.Text = "Descripción Producto";
+            // 
+            // ComboEstado
+            // 
+            ComboEstado.AutoSize = true;
+            ComboEstado.Location = new Point(419, 110);
+            ComboEstado.Name = "ComboEstado";
+            ComboEstado.Size = new Size(42, 15);
+            ComboEstado.TabIndex = 8;
+            ComboEstado.Text = "Estado";
+            // 
+            // TxtPrioridad
+            // 
+            TxtPrioridad.Location = new Point(419, 172);
+            TxtPrioridad.Name = "TxtPrioridad";
+            TxtPrioridad.Size = new Size(212, 23);
+            TxtPrioridad.TabIndex = 11;
+            // 
+            // LabelPrioridad
+            // 
+            LabelPrioridad.AutoSize = true;
+            LabelPrioridad.Location = new Point(419, 154);
+            LabelPrioridad.Name = "LabelPrioridad";
+            LabelPrioridad.Size = new Size(55, 15);
+            LabelPrioridad.TabIndex = 10;
+            LabelPrioridad.Text = "Prioridad";
+            // 
+            // TxtPosicion
+            // 
+            TxtPosicion.Location = new Point(419, 216);
+            TxtPosicion.Name = "TxtPosicion";
+            TxtPosicion.Size = new Size(212, 23);
+            TxtPosicion.TabIndex = 13;
+            // 
+            // LabelPosicion
+            // 
+            LabelPosicion.AutoSize = true;
+            LabelPosicion.Location = new Point(419, 198);
+            LabelPosicion.Name = "LabelPosicion";
+            LabelPosicion.Size = new Size(52, 15);
+            LabelPosicion.TabIndex = 12;
+            LabelPosicion.Text = "Posición";
+            // 
+            // TxtEspacioDisponible
+            // 
+            TxtEspacioDisponible.Location = new Point(419, 260);
+            TxtEspacioDisponible.Name = "TxtEspacioDisponible";
+            TxtEspacioDisponible.Size = new Size(212, 23);
+            TxtEspacioDisponible.TabIndex = 15;
+            // 
+            // LabelEspacioDisponible
+            // 
+            LabelEspacioDisponible.AutoSize = true;
+            LabelEspacioDisponible.Location = new Point(419, 242);
+            LabelEspacioDisponible.Name = "LabelEspacioDisponible";
+            LabelEspacioDisponible.Size = new Size(106, 15);
+            LabelEspacioDisponible.TabIndex = 14;
+            LabelEspacioDisponible.Text = "Espacio Disponible";
+            // 
+            // BotonVolver
+            // 
+            BotonVolver.Location = new Point(505, 381);
+            BotonVolver.Name = "BotonVolver";
+            BotonVolver.Size = new Size(188, 23);
+            BotonVolver.TabIndex = 16;
+            BotonVolver.Text = "Volver";
+            BotonVolver.UseVisualStyleBackColor = true;
+            // 
+            // BotonBuscar
+            // 
+            BotonBuscar.Location = new Point(117, 381);
+            BotonBuscar.Name = "BotonBuscar";
+            BotonBuscar.Size = new Size(188, 23);
+            BotonBuscar.TabIndex = 17;
+            BotonBuscar.Text = "&Buscar";
+            BotonBuscar.UseVisualStyleBackColor = true;
+            // 
+            // BotonLimpiar
+            // 
+            BotonLimpiar.Location = new Point(311, 381);
+            BotonLimpiar.Name = "BotonLimpiar";
+            BotonLimpiar.Size = new Size(188, 23);
+            BotonLimpiar.TabIndex = 18;
+            BotonLimpiar.Text = "&Limpiar";
+            BotonLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // ComboBoxEstado
+            // 
+            ComboBoxEstado.FormattingEnabled = true;
+            ComboBoxEstado.Location = new Point(419, 128);
+            ComboBoxEstado.Name = "ComboBoxEstado";
+            ComboBoxEstado.Size = new Size(212, 23);
+            ComboBoxEstado.TabIndex = 19;
+            // 
+            // ConsultarStockForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(ComboBoxEstado);
+            Controls.Add(BotonLimpiar);
+            Controls.Add(BotonBuscar);
+            Controls.Add(BotonVolver);
+            Controls.Add(TxtEspacioDisponible);
+            Controls.Add(LabelEspacioDisponible);
+            Controls.Add(TxtPosicion);
+            Controls.Add(LabelPosicion);
+            Controls.Add(TxtPrioridad);
+            Controls.Add(LabelPrioridad);
+            Controls.Add(ComboEstado);
+            Controls.Add(TxtDescripcionProducto);
+            Controls.Add(LabelDescripcionProducto);
+            Controls.Add(TxtIDProducto);
+            Controls.Add(LabelIDProducto);
+            Controls.Add(TxtDescripcionCliente);
+            Controls.Add(LabelDescripcionCliente);
+            Controls.Add(TxtIDCliente);
+            Controls.Add(LabelIDCliente);
+            Name = "ConsultarStockForm";
+            Text = "ConsultarStockForm";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label LabelIDCliente;
+        private TextBox TxtIDCliente;
+        private TextBox TxtDescripcionCliente;
+        private Label LabelDescripcionCliente;
+        private TextBox TxtIDProducto;
+        private Label LabelIDProducto;
+        private TextBox TxtDescripcionProducto;
+        private Label LabelDescripcionProducto;
+        private Label ComboEstado;
+        private TextBox TxtPrioridad;
+        private Label LabelPrioridad;
+        private TextBox TxtPosicion;
+        private Label LabelPosicion;
+        private TextBox TxtEspacioDisponible;
+        private Label LabelEspacioDisponible;
+        private Button BotonVolver;
+        private Button BotonBuscar;
+        private Button BotonLimpiar;
+        private ComboBox ComboBoxEstado;
     }
 }
